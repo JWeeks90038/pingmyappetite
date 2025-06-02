3f049dbaf1ba578f756cb01ce8b4012df5063733
JWeeks90038 <jonas.weeks@gmail.com>
2025-06-02 06:21:10 -0700

    Set backend server port to 4242 for consistency

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.get("/api/example", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4242;
 app.listen(PORT, () => {
    // console.log(`Server is running on http://localhost:${PORT}`);
 });
