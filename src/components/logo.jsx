50532107715d0f11646d0e3435e09a14966474f3
JWeeks90038 <jonas.weeks@gmail.com>
2025-06-04 16:31:17 -0700

    Update logo to use PNG and adjust logo styles

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -9,7 +9,6 @@ const Logo = () => {
         src={logoPng}
         alt="Grubana Logo"
         className="logo-img"
-        style={{ maxWidth: '100%', height: 'auto' }}
       />
     </div>
   );
