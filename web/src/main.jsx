4222544e4ab9faa4761d6b92231a3b6ab3d61d09
JWeeks90038 <jonasweeks.dev@gmail.com>
2025-09-16 21:56:31 -0700

    Add styles.css import to enable dark theme and color scheme updates

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./components/AuthContext";
+import "./assets/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
